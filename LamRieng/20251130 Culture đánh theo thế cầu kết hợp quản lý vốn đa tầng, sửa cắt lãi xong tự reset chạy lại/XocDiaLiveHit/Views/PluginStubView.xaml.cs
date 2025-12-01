46df92d9a2e8b84ed589a5f0edd83aed36d3ebe3
ngomantri1 <automino761@gmail.com>
2025-12-01 19:32:58 +0700

    v19 làm riêng

---
+++ LamRieng/20251130 Culture đánh theo thế cầu kết hợp quản lý vốn đa tầng, sửa cắt lãi xong tự reset chạy lại/XocDiaLiveHit/Views/PluginStubView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace XocDiaLiveHit.Views
+{
+    public partial class PluginStubView : UserControl
+    {
+        public PluginStubView()
+        {
+            InitializeComponent();
+        }
+    }
+}
